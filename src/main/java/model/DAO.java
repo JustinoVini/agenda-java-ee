b2c6e4c93d65dbd9eb8567b04e6fb7deba63f828
daiane.peite@hotmail.com <visilva5690@hotmail.com>
2022-09-12 22:09:06 -0300

    modificacao DAO

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,37 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	// Modulo de conexão 
+	// Parametros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = 
+			"jdbc:mysql://localhost:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String username = "root";
+	private String password = "@Viking569038";
+	
+	// Metodo de conexão
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, username, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	// Teste de conexão 
+	public void testeConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
